362e8084f883af9b6f20c0f91084d25580c512b0
Adam Bouqdib <adam@abemedia.co.uk>
2023-12-02 17:12:30 +0000

    build: update to sparkle v2.5.1 (#8)

--- scripts/update_sparkle.sh
+++ scripts/update_sparkle.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
-VERSION="2.4.2"
+VERSION="2.5.1"
+DIR=$(mktemp -d)
 
-wget -O sparkle.tar.xz "https://github.com/sparkle-project/Sparkle/releases/download/$VERSION/Sparkle-$VERSION.tar.xz"
-mkdir sparkle
-tar -xf sparkle.tar.xz --directory ./sparkle
-rm -f sparkle.tar.xz
+wget -q -O $DIR/Sparkle-$VERSION.tar.xz "https://github.com/sparkle-project/Sparkle/releases/download/$VERSION/Sparkle-$VERSION.tar.xz"
+tar -xf $DIR/Sparkle-$VERSION.tar.xz --directory $DIR
 
 find Sparkle.framework -type f ! -name '*.go' -delete
-cp -Lr sparkle/Sparkle.framework/Sparkle Sparkle.framework/
-cp -Lr sparkle/Sparkle.framework/Headers Sparkle.framework/
-cp -Lr sparkle/Sparkle.framework/PrivateHeaders Sparkle.framework/
-rm -rf sparkle
+cp -Lr $DIR/Sparkle.framework/Sparkle Sparkle.framework/
+cp -Lr $DIR/Sparkle.framework/Headers Sparkle.framework/
+cp -Lr $DIR/Sparkle.framework/PrivateHeaders Sparkle.framework/
+rm -rf $DIR
 
-sed -i "s#Sparkle/releases/.*>#Sparkle/releases/$VERSION>#g" README.md
+sed -i '' "s#Sparkle/releases/.*>#Sparkle/releases/$VERSION>#g" README.md
